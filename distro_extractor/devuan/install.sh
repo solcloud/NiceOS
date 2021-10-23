0d6473d97741ba3422116ff1aaf43839ff8a99b1
Daniel Maixner <maixner@solidis.cz>
2021-10-23 18:10:28 +0200

    Minimal devuan variant

--- distro_extractor/devuan/install.sh
+++ distro_extractor/devuan/install.sh
@@ -3,7 +3,7 @@ echo "Starting installer"
 mkfs.ext4 -F /dev/sda
 mount /dev/sda /mnt/
 
-debootstrap --merged-usr --arch=amd64 --include="$(cat /tmp/packages | xargs | sed 's/ /,/g')" chimaera /mnt
+debootstrap --variant=minbase --merged-usr --arch=amd64 --include="$(cat /tmp/packages | xargs | sed 's/ /,/g')" chimaera /mnt
 
 chroot /mnt /bin/bash -c '
 
