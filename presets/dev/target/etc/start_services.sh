1664cc5793815ab1af9ee2cab8d14e390404594f
solcloud <74121353+solcloud@users.noreply.github.com>
2021-11-16 12:55:14 +0100

    Dev machine faster service startup

--- presets/dev/target/etc/start_services.sh
+++ presets/dev/target/etc/start_services.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sleep 4 && sh -c 'while true; do dockerd --data-root /data/images > /dev/null 2>& 1; sleep 30; done' &
-sleep 4 && sh -c 'while true; do /bin/sshd -D > /dev/null 2>& 1; sleep 30; done' &
+sleep 2 && sh -c 'while true; do dockerd --data-root /data/images > /dev/null 2>& 1; sleep 30; done' &
+sleep 2 && sh -c 'while true; do /bin/sshd -D > /dev/null 2>& 1; sleep 30; done' &
 
 wait
