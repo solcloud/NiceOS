b2bf7d421a288ab00affc1919d5e20e4adecf0eb
Daniel Maixner <maixner@solidis.cz>
2021-12-29 13:02:37 +0100

    Bump linux version to longterm 5.10.89

--- _config.sh
+++ _config.sh
@@ -8,7 +8,7 @@ export TARGET_USER=dan
 export TARGET_GROUP=code
 export MAKE_NUM_OF_THREATS='6'
 
-export LINUX_VERSION='5.14.18'
+export LINUX_VERSION='5.10.89'
 export BUSYBOX_VERSION='1.34.1'
 
 export QEMU_NPROC=1
