b68d08b8b7969ce5e96869fb68dbd31a4ec88bf4
Daniel Maixner <maixner@solidis.cz>
2021-12-31 00:05:58 +0100

    preset: dev create /dev/std{in,out,err} symlinks
    why udev dont add them?

--- presets/dev/target/etc/start_services.sh
+++ presets/dev/target/etc/start_services.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+ln -s /proc/self/fd /dev/fd
+ln -s /proc/self/fd/0 /dev/stdin
+ln -s /proc/self/fd/1 /dev/stdout
+ln -s /proc/self/fd/2 /dev/stderr
+
 sleep 2 && sh -c 'while true; do dockerd --data-root /data/images > /dev/null 2>& 1; sleep 30; done' &
 sleep 2 && sh -c 'while true; do /bin/sshd -D > /dev/null 2>& 1; sleep 30; done' &
 
