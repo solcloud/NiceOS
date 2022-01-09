09f2689b3594801d76dec9cb80e2ae3bacc25af3
Daniel Maixner <maixner@solidis.cz>
2022-01-09 18:16:46 +0100

    preset: ghost remove default and empty udev rules

--- presets/ghost/target/root/first_boot_install.sh
+++ presets/ghost/target/root/first_boot_install.sh
@@ -141,6 +141,11 @@ chmod g+rwXs /home/code/src/
 ln -s /home/code/src/solcloud/dev-stack/bin/dev-stack-remote.sh /usr/bin/dev
 echo 'load-module module-echo-cancel' >> /etc/pulse/default.pa
 
+rm -rf /lib/udev/rules.d/
+pushd /etc/udev/rules.d/
+rm $(grep -l '# Intentionally empty' * | xargs)
+popd
+
 echo "New password for daniel"
 passwd daniel
 echo "New password for dan"
