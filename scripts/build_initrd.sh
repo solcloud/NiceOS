d70fac9306e870078c3956721120e66e992f250d
Daniel Maixner <maixner@solidis.cz>
2022-01-22 16:05:58 +0100

    initrd: gzip use long option

--- scripts/build_initrd.sh
+++ scripts/build_initrd.sh
@@ -14,5 +14,5 @@ ln -s busybox bin/sh 2> /dev/null || true
 chmod u+x init || dd "Init not found in $INITRAMFS_BUILD/init"
 
 echo "Creating initramfs cpio archive"
-find . -print0 | cpio --null -ov --format=newc | gzip -9 > $BUILDS/initramfs.cpio.gz # TODO performance check for best algo or no compression at all
+find . -print0 | cpio --null -ov --format=newc | gzip --best > $BUILDS/initramfs.cpio.gz # TODO performance check for best algo or no compression at all
 cp -f $BUILDS/initramfs.cpio.gz $TARGET/boot/initrd
