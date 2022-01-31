e99c2929a596de5c9a2a43ef68bdaaf3b60d941b
Daniel Maixner <maixner@solidis.cz>
2022-01-31 12:40:38 +0100

    Linux, busybox tar with progress instead of verbose

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -9,7 +9,7 @@ mkdir -p "$LINUX_BUILD"
 cd "$LINUX_BUILD"
 
 [ -d "$LINUX_SRC" ] || {
-    tar -xvf "$LINUX" || dd "Missing linux kernel src $LINUX - try run 'make download'"
+    tar --checkpoint=300 -xf "$LINUX" || dd "Missing linux kernel src $LINUX - try run 'make download'"
     cd "$LINUX_SRC"
     make mrproper
 }
