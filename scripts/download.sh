74e21ef249fb5875a478c2f27039cb46ca07c631
Daniel Maixner <maixner@solidis.cz>
2022-01-31 12:59:30 +0100

    Make download, wget use dot:mega styles

--- scripts/download.sh
+++ scripts/download.sh
@@ -5,7 +5,7 @@ source ./.config.sh
 
 # Linux
 LINUX_MAJOR_VERSION=$(echo "$LINUX_VERSION" | grep -o -E '^[1-9]+')
-[ -f "$LINUX" ] || wget -O "$LINUX" "https://cdn.kernel.org/pub/linux/kernel/v${LINUX_MAJOR_VERSION}.x/linux-${LINUX_VERSION}.tar.xz" || rm "$LINUX"
+[ -f "$LINUX" ] || wget --progress=dot:mega -O "$LINUX" "https://cdn.kernel.org/pub/linux/kernel/v${LINUX_MAJOR_VERSION}.x/linux-${LINUX_VERSION}.tar.xz" || rm "$LINUX"
 
 # Busybox
-[ -f "$BUSYBOX" ] || wget -O "$BUSYBOX" "https://busybox.net/downloads/busybox-${BUSYBOX_VERSION}.tar.bz2" || rm "$BUSYBOX"
+[ -f "$BUSYBOX" ] || wget --progress=dot:mega -O "$BUSYBOX" "https://busybox.net/downloads/busybox-${BUSYBOX_VERSION}.tar.bz2" || rm "$BUSYBOX"
