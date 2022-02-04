fe69e261e1cd2409068b87a28111c44083147068
Daniel Maixner <maixner@solidis.cz>
2022-02-04 12:39:51 +0100

    Workflow grouping log

--- run_tests.sh
+++ run_tests.sh
@@ -6,9 +6,17 @@ echo 'sleep 2 && poweroff' > target/etc/start_services.sh
 chmod u+x target/etc/start_services.sh
 
 export NICE_PRESET=base
+echo "::group::Make download"
 make download
+echo "::endgroup::"
+
+echo "::group::Make build"
 make build
+echo "::endgroup::"
+
+echo "::group::Run QEMU"
 make cmd
+echo "::endgroup::"
 
 rm target/etc/start_services.sh
 echo "Tests passed"
