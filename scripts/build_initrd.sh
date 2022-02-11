2c20aa41cc2518a68e54ed0e428120c0049ea20e
Daniel Maixner <maixner@solidis.cz>
2022-02-11 17:43:00 +0100

    Initrd error tuning

--- scripts/build_initrd.sh
+++ scripts/build_initrd.sh
@@ -3,15 +3,13 @@
 set -e
 source ./.config.sh || exit 1
 
-echo "Building Initramfs"
+echo "Building initramfs"
+cd "$INITRAMFS_BUILD" || dd "Cannot cd inside initramfs root folder '$INITRAMFS_BUILD'"
 
-mkdir -p "$INITRAMFS_BUILD"
-cd "$INITRAMFS_BUILD"
-
+chmod u+x init || dd "Init not found in $INITRAMFS_BUILD/init"
 mkdir -p bin/ dev/ etc/ proc/ sys/ mnt/
 cp -f "$BUSYBOX_SRC/busybox" 'bin/busybox'
 ln -s busybox bin/sh 2> /dev/null || true
-chmod u+x init || dd "Init not found in $INITRAMFS_BUILD/init"
 
 echo "Creating initramfs cpio archive"
 find . -print0 | cpio --null -ov --format=newc | gzip --best > "$BUILDS/initramfs.cpio.gz" # TODO performance check for best algo or no compression at all
