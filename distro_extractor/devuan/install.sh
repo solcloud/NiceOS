c50c2a9af1fd0bdb4d3cabf44d65ab6c036280fa
Daniel Maixner <maixner@solidis.cz>
2022-02-22 19:41:58 +0100

    devuan: improve extraction

--- distro_extractor/devuan/install.sh
+++ distro_extractor/devuan/install.sh
@@ -4,26 +4,25 @@ mkfs.ext4 -m 0 -F /dev/sda
 mount /dev/sda /mnt/
 
 arch='amd64'
-debootstrap --variant=minbase --merged-usr --arch="$arch" --exclude="linux-image-$arch,linux-image-rt-$arch" --include="$(cat /tmp/packages.deb.txt | xargs | sed 's/ /,/g')" chimaera /mnt
+suite='chimaera'
+debootstrap --cache-dir=/tmp --variant=minbase --merged-usr --arch="$arch" "$suite" /mnt
+cat /tmp/packages.deb.txt | xargs > /mnt/tmp/packages
 
 chroot /mnt /bin/sh -c '
+apt-get update
+apt-get --fix-broken install --assume-yes
+apt-get --fix-missing install --assume-yes
+apt-get install --no-install-recommends --assume-yes $(cat /tmp/packages)
 
 echo "Cleaning installation"
-apt-get update
-apt-get --fix-broken install -y
-apt-get --fix-missing install -y
-
-# get rid off /sbin
+# Get rid off /sbin
 mv /usr/sbin/* /usr/bin/
 rm /usr/sbin/*
 rmdir /usr/sbin/
 ln -s /usr/bin /usr/sbin
 
-pushd /usr/bin
-    rm -f iptables
-    ln -s iptables-legacy iptables
-popd
-
+rm -f /usr/bin/iptables
+ln -s /usr/bin/iptables-legacy /usr/bin/iptables
 
 '
 
