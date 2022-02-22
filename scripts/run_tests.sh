0bf3d6a168461df08a74367cd2a5daef4fd21fde
Daniel Maixner <maixner@solidis.cz>
2022-02-22 19:15:43 +0100

    tests: add niceOS prefix

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,8 +3,8 @@
 set -e
 
 export NICE_PRESET=base
-export VM_MOUNT_ROOT=/tmp/extract_stub
-export NICE_EXTRACT_DISTRO_HDD_IMAGE_PATH=/tmp/not-exists.noimg
+export VM_MOUNT_ROOT=/tmp/nice_os_extract_stub
+export NICE_EXTRACT_DISTRO_HDD_IMAGE_PATH=/tmp/nice-not-exists.noimg
 
 cp storage/test_service.sh target/etc/start_services.sh
 chmod u+x target/etc/start_services.sh
