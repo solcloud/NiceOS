b61ed61c5118df50f39fa2466a9898c76c38fba0
Daniel Maixner <maixner@solidis.cz>
2022-02-26 14:11:11 +0100

    extract: Add Void Linux

---
+++ distro_extractor/void/inc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PM=xbps
+VM_USER=anon
+VM_PASS=voidlinux
+
+function boot_info() {
+    echo "Go to $DISTRO window"
+    echo "Start boot by pressing enter or wait 10 sec"
+    echo "After boot finished"
+}
---
+++ distro_extractor/void/install.sh
@@ -0,0 +1,19 @@
+echo "Starting installer"
+mkfs.ext4 -m 0 -F /dev/sda
+mount /dev/sda /mnt/
+
+# Copy keys from live CD
+mkdir -p /mnt/var/db/xbps/keys
+cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
+
+arch='x86_64'
+repository='https://alpha.de.repo.voidlinux.org/current'
+XBPS_ARCH=$arch xbps-install --yes -S -r /mnt -R "$repository" $(cat /tmp/packages.xbps.txt | xargs)
+chroot /mnt /bin/sh -c '
+echo "Cleaning installation"
+
+'
+
+sync
+umount /mnt/
+poweroff
