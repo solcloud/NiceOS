5abc385ea89743132db4708e13e13daad2fd72b5
Daniel Maixner <maixner@solidis.cz>
2022-03-17 12:58:36 +0100

    target: udev refresh lower kill timeout

--- target/etc/init/udev_refresh.sh
+++ target/etc/init/udev_refresh.sh
@@ -1,4 +1,4 @@
 udevd --daemon 2> /dev/null
 udevadm trigger --action=add --type=subsystems
 udevadm trigger --action=add --type=devices
-sleep 20 && udevadm control --exit &
+sleep 10 && udevadm control --exit &
