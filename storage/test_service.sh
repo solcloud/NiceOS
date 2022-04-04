586ebd1918843917ff5870f2b66847f10cdc5a3d
Daniel Maixner <maixner@solidis.cz>
2022-04-04 16:52:24 +0200

    base: more tests

--- storage/test_service.sh
+++ storage/test_service.sh
@@ -6,8 +6,12 @@ sleep 2
 cat /usr/test.txt
 test -e /usr/src/niceOS.hash
 test -e /usr/src/rebuild_and_reinstall_linux.sh
+test -e /etc/preset_sync_test
+test -L /etc/preset_sync_symlink_test
 test "$(hostname)" = 'base'
 ( echo "test /dev/stdout" > /dev/stdout ) > /dev/null
+stat -c "%a" /bin/busybox | grep -q '7.5'
+grep -q 'base' /etc/hosts
 
 
 poweroff
